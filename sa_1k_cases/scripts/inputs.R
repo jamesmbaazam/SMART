0290a7a77b2570e67c57cf5fc407993ad4a1e431
James Azam <jamesazam@sun.ac.za>
2022-02-15 15:28:12 +0200

    Added the inputs file

---
+++ sa_1k_cases/scripts/inputs.R
@@ -0,0 +1,17 @@
+#Inputs
+
+#Serial interval distribution
+si_sd <- exp(2.9)
+si_mean <- exp(4.7)
+
+
+serial_interval <- function(x = 1) {
+  si <- rlnorm(x, meanlog = si_mean, sdlog = si_sd)
+  return(si)
+  }
+
+chain_lengths <- seq(1, 10, 1)
+
+seed_times <- seq(0, 7, 1) #Assuming time 0 = March 5 and time 7 = March 13
+
+end_times <- seed_times + 14 
