2bd1cef7c60ef6897a4f26641f049cc5b0e69194
James Azam <jamesazam@sun.ac.za>
2022-02-15 15:29:23 +0200

    Renamed the script for running the branching process model

--- sa_1k_cases/scripts/sa_COVID_2wks_project.R
+++ sa_1k_cases/scripts/sa_COVID_2wks_project.R
@@ -1,35 +1,39 @@
+#Packages
 library("bpmodels")
 
+#Source the inputs
+source('./scripts/inputs.R')
+
 # Chain log-likelihood outcomes ----
-?chain_ll
-
-chain_lengths <- c(1, 2, 3, 4, 5)
-
 ll_chain <- chain_ll(
   x = chain_lengths,
   offspring = "pois",
   lambda = 0.5,
-  stat = "size",
-  obs_prob = 1#, # assumes a constant observation probability
-  #nsim_obs = 100
+  stat = "length",
+  individual = FALSE,
+  obs_prob = 1, # assumes a constant observation probability
+  nsim_obs = 10
 )
 
+#output
 ll_chain
 
 plot(ll_chain, type = "l")
 
 ## Chain log-likelihood simulation
-?chain_sim
-
 ll_sim <- chain_sim(
   n = 10,
   offspring = "pois",
   lambda = 0.5,
   stat = "size",
-  tree = FALSE
+  serial = serial_interval,
+  t0 = 0,
+  tf = 14,
+  tree = TRUE
   #nsim_obs = 100
 )
 
+#output
 ll_sim
 
 plot(ll_sim, type = "l")
