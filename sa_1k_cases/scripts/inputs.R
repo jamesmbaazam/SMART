3c936204be88bc6aa39912684bd48f7577a5a050
James Azam <jamesazam@sun.ac.za>
2022-02-21 15:12:18 +0200

    Moved the calculations of the desired mean and sd to the helper script

--- sa_1k_cases/scripts/inputs.R
+++ sa_1k_cases/scripts/inputs.R
@@ -10,9 +10,9 @@ source('./scripts/get_data.R')
 #Get the cleaned data
 dat <- readRDS('./data/sa_covid_upto_mar5_2020.rds')
 
-#Serial interval distribution
-si_sd <- sqrt(log(1 + (2.7/4.7)^2))
-si_mean <- log((4.7^2)/(sqrt(2.7^2 + 4.7^2)))
+#bpmodels inputs
+dat <- dat %>% 
+  mutate(days_from_t0 = as.integer(date - min(date)))
 
 
 serial_interval <- function(x = 1) {
