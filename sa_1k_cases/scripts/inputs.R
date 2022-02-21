ef66f0407928f135bf5b60c24892d7fce405eb65
James Azam <jamesazam@sun.ac.za>
2022-02-21 15:11:43 +0200

    Removed some old inputs we no longer need

--- sa_1k_cases/scripts/inputs.R
+++ sa_1k_cases/scripts/inputs.R
@@ -19,9 +19,3 @@ serial_interval <- function(x = 1) {
   si <- rlnorm(x, meanlog = si_mean, sdlog = si_sd)
   return(si)
   }
-
-chain_lengths <- seq(1, 10, 1)
-
-seed_times <- seq(0, 7, 1) #Assuming time 0 = March 5 and time 7 = March 13
-
-end_times <- seed_times + 14 
