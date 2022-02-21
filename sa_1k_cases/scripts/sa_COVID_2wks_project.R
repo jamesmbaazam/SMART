df83292c734526a7c95217e7c32283479f8a47fa
James Azam <jamesazam@sun.ac.za>
2022-02-21 17:05:16 +0200

    Rewrote the call to chain_sim() to run many sims and bind the results into one df

--- sa_1k_cases/scripts/sa_COVID_2wks_project.R
+++ sa_1k_cases/scripts/sa_COVID_2wks_project.R
@@ -8,30 +8,37 @@ source('./scripts/inputs.R')
 
 set.seed(1234)
 ## Chain log-likelihood simulation
-simulation_output <- chain_sim(
-  n = length(days_from_t0),
-  offspring = "nbinom",
-  mu = 2.0,
-  size = 0.38,
-  stat = "size",
-  infinite = 1E7,
-  serial = serial_interval,
-  t0 = days_from_t0,
-  tf = projection_end_day,
-  tree = TRUE
+simulation_output <- map_dfr(1:number_of_sims,
+  function(id) {
+    out <- chain_sim(
+    n = length(days_from_t0),
+    offspring = "nbinom",
+    mu = 2.0,
+    size = 0.38,
+    stat = "size",
+    infinite = 1E7,
+    serial = serial_interval,
+    t0 = days_from_t0,
+    tf = projection_end_day,
+    tree = TRUE
+    ) %>% 
+      mutate(sim_id = id)
+    return(out)
+    }
 )
 
 #output
 simulation_output_mod <- simulation_output %>% 
   mutate(day = floor(time)) %>% 
-  group_by(day) %>% 
-  summarise(cases = n(), .groups = 'drop') %>% 
-  summarise(cum_cases = cumsum(cases))
+  group_by(sim_id, day) %>% 
+  summarise(cases = n(), .groups = 'drop_last') %>% 
+  summarise(cum_cases = cumsum(cases)) %>% 
+  slice_max(cum_cases) %>% 
+  ungroup()
 
 #make plots
-cases_plot <- ggplot(data = simulation_output) + 
-  geom_line(aes(x = date, y = cum_cases), 
-           stat = 'identity', 
+cases_plot <- ggplot(data = simulation_output_mod) + 
+  geom_boxplot(aes(cum_cases), 
            color = 'blue',
            size = 1
            )
